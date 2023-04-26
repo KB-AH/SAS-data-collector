08e79d3b549080d99800415ffb2a20e5b78ee2d3
Butaforsky <59537160+Butaforsky@users.noreply.github.com>
2023-04-26 13:57:24 +0300

    add comment

--- src/cfg.ts
+++ src/cfg.ts
@@ -1,10 +1,10 @@
 export const COM_PORTS: readonly `COM${number}`[] = [
   // "COM6", // SAS3
-  // "COM7", // SAS1
-  "COM10" // SAS0
+  "COM7", // SAS1
+  //"COM10" // SAS0
 ] as const;
 
-export const slo: 0 | 1 = 1;
-export const rot: 0 | 1 = 0;
+export const slo: 0 | 1 = 1; // slouched, if equal 0, seat straight
+export const rot: 0 | 1 = 0; // rotate
 
 export const outfile = "ma_1_right_1"
